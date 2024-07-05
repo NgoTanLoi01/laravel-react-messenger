04a51019c11d890962b8e77c3142accd1e76706a
NgoTanLoi01 <ngotanloidh2018@gmail.com>
2024-07-05 15:29:17 +0700

    feat: Generate Model fillble and relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'avatar',
         'name',
         'email',
+        'email_verified_at',
         'password',
+        'is_admin'
     ];
 
     /**
@@ -42,4 +45,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function groups () 
+    {
+        return $this ->belongsToMany(Group::class, 'group_users');
+    }
 }
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,10 +4,24 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+import Echo from 'laravel-echo';
 import axios from 'axios';
-window.axios = axios;
 
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    wsHost: import.meta.env.VITE_PUSHER_HOST ?? `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
+    wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
+    forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? 'https') === 'https',
+    enabledTransports: ['ws', 'wss'],
+});
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
